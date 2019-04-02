91a19b22d45ed95bba20a1b2005bd92fb3bb3a9c
Flavio Mello <flaviorafmello@gmail.com>
2019-04-02 21:16:38 +0100

    Consolidate git commands. Move files to default folder

--- default/git.aliases.sh
+++
@@ -1,7 +0,0 @@
-# Undo a `git push`
-alias undopush="git push -f origin HEAD^:master"
-alias apply-gitignore="git ls-files -ci --exclude-standard -z | xargs -0 git rm --cached"
-alias gitk='gitk 2>/dev/null'
-
-# git root
-alias gr='[ ! -z `git rev-parse --show-cdup` ] && cd `git rev-parse --show-cdup || pwd`'
