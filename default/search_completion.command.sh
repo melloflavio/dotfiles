668a65e6493d7d0e3757a0237d1b3b27905d65ba
Flavio Mello <flaviorafmello@gmail.com>
2019-04-02 21:21:30 +0100

    Create search_completion.command.sh

---
+++ default/search_completion.command.sh
@@ -0,0 +1,7 @@
+# https://unix.stackexchange.com/questions/5366/command-line-completion-from-command-history
+
+# Key bindings, up/down arrow searches through history
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
+bind '"\eOA": history-search-backward'
+bind '"\eOB": history-search-forward'
