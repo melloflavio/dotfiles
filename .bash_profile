d3d55e7f9bb807a1f1c62bc03382bf6ff87e0069
Flavio Mello <flaviorafmello@gmail.com>
2019-04-03 00:03:44 +0100

    conditionally load diff systems

--- .bash_profile
+++ .bash_profile
@@ -3,3 +3,6 @@ if [ -f ~/.bashrc ]; then . ~/.bashrc; fi
 
 # Load dotfiles project entrypoint. It takes care of loading the rest
 source ~/bash-dotfiles/entrypoint.sh
+
+# 'windows' | 'osx'
+export BASH_DOTFILE_SYS='windows'
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,15 @@ for file in  ~/bash-dotfiles/default/*.sh; do
   [ -r "$file" ] && source "$file"
 done
 unset file
+
+# Load BASH_DOTFILE_SYS files
+if [ "$BASH_DOTFILE_SYS" = "windows" ] ; then
+    for file in "~/bash-dotfiles/windows/*.sh"; do
+        [ -r "$file" ] && source "$file"
+    done
+elif [ "$BASH_DOTFILE_SYS" ="osx" ] ; then
+    for file in "~/bash-dotfiles/osx/*.sh"; do
+        [ -r "$file" ] && source "$file"
+    done
+fi
+unset file
