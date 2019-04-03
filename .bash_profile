c2b0750ae4d96d32a14dac4c67510594a35f5bd3
Flavio Mello <flaviorafmello@gmail.com>
2019-04-03 20:07:41 +0100

    Create folder for bash scripts

--- .bash_profile
+++ .bash_profile
@@ -2,7 +2,8 @@
 if [ -f ~/.bashrc ]; then . ~/.bashrc; fi
 
 # 'windows' | 'osx'
-export BASH_DOTFILE_SYS='windows'
+export SHELL_DOTFILE_SYS='windows'
+export SHELL_DOTFILE_SHELL='bash'
 
 # Load dotfiles project entrypoint. It takes care of loading the rest
 source ~/bash-dotfiles/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,21 @@ for file in  ~/bash-dotfiles/default/*.sh; do
 done
 unset file
 
-# Load BASH_DOTFILE_SYS files
-if [ "$BASH_DOTFILE_SYS" = "windows" ] ; then
+# Load SHELL_DOTFILE_SYS files
+if [ "$SHELL_DOTFILE_SYS" = "windows" ] ; then
     for file in ~/bash-dotfiles/windows/*.sh; do
         [ -r "$file" ] && source "$file"
     done
-elif [ "$BASH_DOTFILE_SYS" = "osx" ] ; then
+elif [ "$SHELL_DOTFILE_SYS" = "osx" ] ; then
     for file in ~/bash-dotfiles/osx/*.sh; do
+        [ -r "$file" ] && source "$file"
+    done
+fi
+unset file
+
+# Load SHELL_DOTFILE_SHELL files
+if [ "$SHELL_DOTFILE_SHELL" = "bash" ] ; then
+    for file in ~/bash-dotfiles/bash/*.sh; do
         [ -r "$file" ] && source "$file"
     done
 fi
